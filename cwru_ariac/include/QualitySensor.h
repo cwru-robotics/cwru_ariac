4b351d12f703d209406dc4631e0b32169d4f7566
TuZZiX <tianshipei5@126.com>
2017-05-01 19:50:39 -0400

    add ID generator and quality control sensor

---
+++ cwru_ariac/include/QualitySensor.h
@@ -0,0 +1,32 @@
+//
+// Created by rockwell on 5/1/17.
+//
+
+#ifndef CWRU_ARIAC_QUALITYSENSOR_H
+#define CWRU_ARIAC_QUALITYSENSOR_H
+
+#include <AriacBase.h>
+
+class QualitySensor {
+public:
+    QualitySensor(ros::NodeHandle nodeHandle);
+    PartList AGV1badParts;
+    PartList AGV2badParts;
+    void ForceUpdate();
+protected:
+    ros::NodeHandle nh;
+    ros::Subscriber sensor1Subscriber;
+    ros::Subscriber sensor2Subscriber;
+    IDGenerator idGenerator;
+    tf::TransformListener tf_listener;
+    int updateCount;
+    int checkedCount;
+    string worldFrame;
+    string sensor1Frame;
+    string sensor2Frame;
+    void sensor1Callback(const osrf_gear::LogicalCameraImage::ConstPtr & sensor_msg);
+    void sensor2Callback(const osrf_gear::LogicalCameraImage::ConstPtr & sensor_msg);
+};
+
+
+#endif //CWRU_ARIAC_QUALITYSENSOR_H
