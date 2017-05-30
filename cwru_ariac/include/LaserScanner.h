d347ea59bb7810038e3c26c670c1f6d45a4695b2
William Wong <sxh759@case.edu>
2017-05-30 11:39:14 -0400

    Update the laser scanner for the ariac v1.1.2 (#20)
    * New updates tothe laser scanner.
    
    * A few improvements

--- cwru_ariac/include/LaserScanner.h
+++ cwru_ariac/include/LaserScanner.h
@@ -7,17 +7,13 @@
 
 #include <LaserScanner.h>
 
-#include <ros/ros.h> 
+#include <ros/ros.h>
 #include <sensor_msgs/LaserScan.h>
-#include <std_msgs/Float32.h> 
-#include <std_msgs/Float64.h> 
+#include <std_msgs/Float32.h>
+#include <std_msgs/Float64.h>
 #include "std_msgs/String.h"
-#include <std_msgs/Bool.h> 
-#include <vector> 
-
-// #include <laser_scanner/BeltImage.h>
-// #include <laser_scanner/Row.h>
-
+#include <std_msgs/Bool.h>
+#include <vector>
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include <cv_bridge/cv_bridge.h>
@@ -29,7 +25,7 @@
 
 #include <AriacBase.h>
 #include <ariac_xform_utils/ariac_xform_utils.h>
-#include <Eigen/Eigen> 
+#include <Eigen/Eigen>
 
 using namespace std;
 using namespace cv;
@@ -54,9 +50,15 @@ public:
 
 	void type_a_asymmetric(cv::Mat dewarped_mat_a, float scan_height, vector<float>& pin_origin, vector<int>& origin_pixel, int part_id);
 
-	void type_b_stamped_center(cv::Mat dewarped_mat_c, float& t_0, float& t_1, float& t_stamp, ros::Time& ros_t_0, ros::Time& ros_t_1, ros::Time& ros_t_stamp, vector<float>& origin, vector<int>& origin_pixel);
+	// MK1 obsolete
+	// void type_b_stamped_center(cv::Mat dewarped_mat_c, float& t_0, float& t_1, float& t_stamp, ros::Time& ros_t_0, ros::Time& ros_t_1, ros::Time& ros_t_stamp, vector<float>& origin, vector<int>& origin_pixel);
+	// void type_b_asymmetric(cv::Mat dewarped_mat_a, float scan_height, vector<float>& pin_origin, vector<int>& origin_pixel, int part_id);
 
-	void type_b_asymmetric(cv::Mat dewarped_mat_a, float scan_height, vector<float>& pin_origin, vector<int>& origin_pixel, int part_id);
+	// MK2 current
+	void type_b_asymmetric(cv::Mat dewarped_mat_a, float scan_height, vector<float>& pin_origin, vector<int>& pin_pixel, int part_id);
+
+	void type_b_stamped_center(cv::Mat dewarped_mat_c, float scan_height, float& t_0, float& t_1, float& t_stamp, ros::Time& ros_t_0, ros::Time& ros_t_1, ros::Time& ros_t_stamp, vector<float>& origin, vector<int>& pin_pixel, vector<int>& origin_pixel);
+
 
 	void ForceUpdate();
 
@@ -67,7 +69,7 @@ protected:
 	ros::Subscriber belt_speed_subscriber;
 	ros::Subscriber scanner_2_subscriber;
 	ros::Publisher part_publisher;
-	
+
 	int output_flag = 1;
 
 
@@ -76,8 +78,8 @@ protected:
 	void belt_speed_cb(const std_msgs::Float64& msg);
 
 	float ping_dist_= 0.0;
-	int ping_index_min_= -1; 
-	int ping_index_max_ = -1; 
+	int ping_index_min_= -1;
+	int ping_index_max_ = -1;
 	int center_index_ = -1;
 	double phi_ = 0.0;
 	double phi_temp;
@@ -92,17 +94,15 @@ protected:
 	bool prev_part_on_belt_ = false;
 
 	std::vector<bool> vec_part_on_belt;
-	float belt_depth_ = 1.6; 
-	double tolerance_ = 0.005; 
-
+	float belt_depth_ = 1.6;
+	double tolerance_ = 0.005;
 	// laser_scanner::BeltImage part_;
-
 	int item_count = 0;
 	std::vector<float> row_z;
-	std::vector<float> row_z_prev_0; 
-	std::vector<float> row_z_prev_1; 
-	std::vector<float> row_z_prev_2; 
-	std::vector<float> row_z_prev_3; 
+	std::vector<float> row_z_prev_0;
+	std::vector<float> row_z_prev_1;
+	std::vector<float> row_z_prev_2;
+	std::vector<float> row_z_prev_3;
 	float time_0 = 0;
 	float time0_0 = 0;
 	float time0_1 = 0;
@@ -126,20 +126,30 @@ protected:
 	float belt_speed = 0;
 	float speed_temp_0 = 0;
 	float speed_temp_1 = 0;
-	float rod_sqr_l = 0.052; // 0.052 x 0.059
+	// float rod_sqr_l = 0.052; // 0.052 x 0.059
+	float rod_sqr_l = 0.0556; // 0.052 x 0.059 rectangle diagonal
 
 	const float ht_disk = 0.02316;
 	const float ht_gasket = 0.02;
 	const float ht_gear = 0.012;
-	const float ht_piston_rod = 0.0075; 
+	const float ht_piston_rod = 0.0075;
 
 	std::vector<float> laser_profiler_2_origin; // RN's own config TODO update to CWRU's config
 
 	//For ForceUpdate();
 	int updateCount_cb_speed;
-	int checkedCount_cb_speed;	
+	int checkedCount_cb_speed;
 	int updateCount_cb_scan;
 	int checkedCount_cb_scan;
+
+	// For LaserScanner::type_b_stamped_center()
+	// int square_l;
+	// int window_r;
+	// float window_r_float;
+	// std::vector<float> theta;
+	// std::vector<int> width_delta;
+	// vector<float> debug;
+	// int temp_b_c;
 
 
 };
